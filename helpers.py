c154a1b0815807243a6268a462a56b7f81976719
Daniel <91837597+JackDouniels@users.noreply.github.com>
2024-11-10 15:29:32 +0100

    First modifications on analysis

--- helpers.py
+++ helpers.py
@@ -29,26 +29,26 @@ def txt_to_csv(file_txt, file_csv, dataset):
     if dataset == "BA":
          features.append("review")
 
-    # If the file already exist, just delete it
+    # if the file already exist, just delete it
     if os.path.exists(file_csv):
         os.remove(file_csv)
 
-    # Let start by oppening the input and output files
+    # let start by oppening the input and output files
     with open(file_txt, 'r', encoding='utf-8') as file:
         with open(file_csv, 'a', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
 
-            # Start by writing the features row
+            # start by writing the features row
             writer.writerow(features)
 
-            # Take line by line
+            # take line by line
             for line in file:
                 line = line.strip()
 
-                # If the line is empty, change the rating element
+                # if the line is empty, change the rating element
                 if line == "":
 
-                        # Write the rating elements each 10'000
+                        # write the rating elements each 10'000
                         if iteration % 10000 == 0:
                             writer.writerows(data)
                             data = []
@@ -58,14 +58,14 @@ def txt_to_csv(file_txt, file_csv, dataset):
                         iteration += 1
                         print(f"Iteration : {iteration}")
                         data_line = []
-                # Else add the feature to the in growing line, and supress the feature prefix
+                # else add the feature to the in growing line, and supress the feature prefix
                 else:
                     parts = line.split(": ")
                     if parts[0] == "text:":
                         data_line.append("")
                     else:
                         data_line.append(parts[1])
-            # Add the remaining data to the csv file
+            # add the remaining data to the csv file
             if data:
                     writer.writerows(data)
 
@@ -86,28 +86,28 @@ def ploting_country_representation(BA, RB, scale):
     sns.set_theme(style="whitegrid")
     figure, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 6), sharey=True)
 
-    # Bar plot for BA
+    # bar plot for BA
     BA.plot(kind='bar', ax=axes[0], color=sns.color_palette("Blues", n_colors=len(BA))[::-1])
     axes[0].set_xlabel('Countries', fontsize=12)
-    axes[0].set_ylabel('Number of Ratings', fontsize=12)
-    axes[0].set_title('Top 25 Countries by Ratings (BeerAdvocate)', fontsize=14)
+    axes[0].set_ylabel('Number of Ratings', fontsize=10)
+    axes[0].set_title('Top 25 Countries by Ratings (BeerAdvocate) in log scale', fontsize=14)
     axes[0].set_yscale(scale)
 
-    # Add text that define the value
+    # add text that define the value
     for index, value in enumerate(BA):
         axes[0].text(index, value, f'{value}', ha='center', va='bottom', fontsize=8)
 
-    # Bar plot for RB
+    # bar plot for RB
     RB.plot(kind='bar', ax=axes[1], color=sns.color_palette("Reds", n_colors=len(RB))[::-1])
-    axes[1].set_xlabel('Countries', fontsize=12)
-    axes[1].set_title('Top 25 Countries by Ratings (RateBeer)', fontsize=14)
+    axes[1].set_xlabel('Countries', fontsize=10)
+    axes[1].set_title('Top 25 Countries by Ratings (RateBeer) in log scale', fontsize=14)
     axes[1].set_yscale(scale)
 
-    # Add text that define the value
+    # add text that define the value
     for index, value in enumerate(RB):
         axes[1].text(index, value, f'{value}', ha='center', va='bottom', fontsize=8)
 
-    # Placing the labels
+    # placing the labels
     for ax in axes:
         ax.tick_params(axis='x', rotation=90)
         ax.grid(axis='y', linestyle='--', alpha=0.7)
@@ -128,13 +128,13 @@ def ploting_user_ratings_distribution(BA_ratings_bins, RB_ratings_bins):
 	
 	plt.figure(figsize=(15, 6))
 
-	# Add the data for BA in plot
+	# add the data for BA in plot
 	BA_ratings_bins.plot(kind='bar', color='skyblue', edgecolor='black', alpha=1, label="BA Users", position=1, width=0.4)
 
-	# Add the data for RB in plot
+	# add the data for RB in plot
 	RB_ratings_bins.plot(kind='bar', color='salmon', edgecolor='black', alpha=1, label="RB Users", position=0, width=0.4)
 
-	# Define labels, legend and titles
+	# define labels, legend and titles
 	plt.title("Distribution of users by number of ratings", fontsize=14)
 	plt.xlabel("# Ratings", fontsize=12)
 	plt.ylabel("# Users", fontsize=12)
@@ -143,16 +143,16 @@ def ploting_user_ratings_distribution(BA_ratings_bins, RB_ratings_bins):
 	plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x):,}'))  # Format y labels
 	plt.legend()
 
-	# Rotate and define size for ticks
+	# rotate and define size for ticks
 	plt.xticks(rotation=90)
 	plt.xticks(fontsize=10)
 	plt.yticks(fontsize=11)
 
-	# Little change on placement
+	# little change on placement
 	plt.tight_layout()
 	plt.xlim(-0.7)
 	
-	# Show the plot
+	# show the plot
 	plt.show()
 
 """
@@ -169,7 +169,7 @@ def ploting_ratings_by_month(BA_ratings_by_month, RB_ratings_by_month, scale):
 
 	fig, axes = plt.subplots(1, 2, figsize=(15, 7))
 
-	# Define first graph (BA) parameters
+	# define first graph (BA) parameters
 	axes[0].plot(BA_ratings_by_month['rating_date'], BA_ratings_by_month['#ratings'], marker='.', linestyle='None', color='blue', markerfacecolor='blue')
 	axes[0].set_title('Number of ratings each month', fontsize=14)
 	axes[0].set_xlabel('Year', fontsize=12)
@@ -181,7 +181,7 @@ def ploting_ratings_by_month(BA_ratings_by_month, RB_ratings_by_month, scale):
 	axes[0].grid(True, axis='x', linestyle='--', linewidth=0.5)
 	axes[0].grid(True, axis='y', linestyle='--', linewidth=0.5)
 
-	# Define second graph (RB) parameters
+	# define second graph (RB) parameters
 	axes[1].plot(RB_ratings_by_month['rating_date'], RB_ratings_by_month['#ratings'], marker='.', linestyle='None', color='blue', markerfacecolor='blue')
 	axes[1].set_title('Number of ratings each month', fontsize=14)
 	axes[1].set_xlabel('Year', fontsize=12)
@@ -193,10 +193,10 @@ def ploting_ratings_by_month(BA_ratings_by_month, RB_ratings_by_month, scale):
 	axes[1].grid(True, axis='x', linestyle='--', linewidth=0.5)
 	axes[1].grid(True, axis='y', linestyle='--', linewidth=0.5)
 
-	# Little change on placement
+	# little change on placement
 	plt.tight_layout()
 
-	# Show the plots
+	# show the plots
 	plt.show()
 
 
